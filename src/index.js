4cc993b2169ef242eee23396fe796e879b59523e
Nikita Shabulin <nwpress@yandex.ru>
2020-12-29 13:37:53 +0700

    redux-logger installed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './styles.css';
-import {createStore} from './createStore';
+import {applyMiddleware, createStore} from 'redux';
+import thunk from 'redux-thunk';
 import {rootReducer} from './redux/rootReducer';
+import {asyncIncrement, decrement, increment} from './redux/actions';
 
 const counter = document.getElementById('counter');
 const addBtn = document.getElementById('add');
@@ -8,18 +10,30 @@ const subBtn = document.getElementById('sub');
 const asyncBtn = document.getElementById('async');
 const themeBtn = document.getElementById('theme');
 
-const store = createStore(rootReducer, 0);
+function logger(state) {
+  return function (next) {
+    return function (action) {
+      return next(action);
+    };
+  };
+}
+
+const store = createStore(
+  rootReducer,
+  0,
+  applyMiddleware(thunk, logger)
+);
 
 addBtn.addEventListener('click', () => {
-  store.dispatch({type: 'INCREMENT'});
+  store.dispatch(increment());
 });
 
 subBtn.addEventListener('click', () => {
-  store.dispatch({type: 'DECREMENT'});
+  store.dispatch(decrement());
 });
 
 asyncBtn.addEventListener('click', () => {
-
+  store.dispatch(asyncIncrement());
 });
 
 store.subscribe(() => {
